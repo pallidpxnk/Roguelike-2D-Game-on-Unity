a8940028312d17abc39c6067f4dca95915464b55
pallidpxnk <k.holovanchuk@icloud.com>
2023-11-24 02:46:43 +0200

    Added Player, Controlling the Player and shooting

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+    public Rigidbody2D theRB;
+    public Transform gunArm;
+    private Camera theCam;
+    public Animator anim;
+    public GameObject bulletToFire;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = Camera.main;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        theRB.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if (mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3 (-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        } else
+        {
+            transform.localScale = new Vector3(1f,1f,1f);
+            gunArm.localScale = new Vector3(1f,1f,1f);
+        }
+
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            shotCounter = timeBetweenShots;
+        }
+        if(Input.GetMouseButton(0)) 
+        {
+            shotCounter -= Time.deltaTime;
+
+            if(shotCounter <= 0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+
+                shotCounter = timeBetweenShots;
+            }
+        }
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
